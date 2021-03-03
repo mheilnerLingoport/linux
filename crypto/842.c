188ef1a6f55f30b348abb85231f3b3426fdbb6c0
mheilnerLingoport <mheilner@lingoport.com>
2021-03-03 10:35:53 -0700

    MH

--- crypto/842.c
+++ crypto/842.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Cryptographic API for the 842 software compression algorithm. MARY
+ * Cryptographic API for the 842 software compression algorithm. MARY HEILNER
  *
  * Copyright (C) IBM Corporation, 2011-2015
  *
